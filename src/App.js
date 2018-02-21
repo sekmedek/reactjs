ab2be64bc251645d309ead8fe9f07bc02d746697
SEKMEDEK <rmboransing@gmail.com>
2018-02-21 22:44:23 +0800

    Added greeting.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          My name is SEKMEDEK.
+          Hello, my name is SEKMEDEK.
         </p>
       </div>
     );
